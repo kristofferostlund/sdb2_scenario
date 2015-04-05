9fafbb728800dbf812e72b6a1cf6e3000da8e0b2
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-04-06 00:24:36 +0100

    Remove bin

--- bin/sdb2_scenario.py
+++
@@ -1,4 +0,0 @@
-# Author: Kristoffer Ostlund
-# Website: http://kristofferostlund.com/
-# Git Repo: https://github.com/kristofferostlund/sdb2_scenario
-# Copyright (C) 2015 Kristoffer Ostlund. All rights reserved.
