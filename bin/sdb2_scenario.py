a48f9c15166f1c5e8b0e83f48282790a9042d990
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-04-05 22:12:46 +0100

    Add folder structure

---
+++ bin/sdb2_scenario.py
@@ -0,0 +1,4 @@
+# Author: Kristoffer Ostlund
+# Website: http://kristofferostlund.com/
+# Git Repo: https://github.com/kristofferostlund/sdb2_scenario
+# Copyright (C) 2015 Kristoffer Ostlund. All rights reserved.
